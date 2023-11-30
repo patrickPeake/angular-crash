a748223c64df834fedb25899ebd6c4620a450c14
Patrick <pjp3@sfu.ca>
2023-11-29 21:55:20 -0800

    where modules.ts

--- angular-crash/src/app/app.component.ts
+++ angular-crash/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+//import { HeaderComponent } from './components/header/header.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
